0e8e542e5f2b765b8bf334a2e8e7c4199381786f
lferng17 <lferng17@estudiantes.unileon.es>
2023-11-16 16:26:11 +0100

    Redirección historial

--- frontend/src/app/components/historial/historial.component.ts
+++ frontend/src/app/components/historial/historial.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalizadorSeoService } from 'src/app/services/analizador-seo.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-historial',
   templateUrl: './historial.component.html',
-  styleUrls: ['./historial.component.css']
+  styleUrls: ['./historial.component.css'],
+  providers: [DatePipe]
 })
 export class HistorialComponent implements OnInit{
 
